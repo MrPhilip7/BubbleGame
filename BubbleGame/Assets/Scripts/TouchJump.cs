70cd9d0c44a822c3b7ede80890552905cd1f31ef
MrPhilip7 <72625726+MrPhilip7@users.noreply.github.com>
2021-06-12 18:37:03 +0200

    [BUB-3] Konfiguracja poruszania

---
+++ BubbleGame/Assets/Scripts/TouchJump.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchJump : MonoBehaviour {
+    private Rigidbody2D rb;
+    public float jumpForce = 600f;
+
+    // Start is called before the first frame update
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Force);
+        }
+    }
+}
