d905a8d4cd5634d47b7bc7c16fd107c81a476472
MrPhilip7 <72625726+MrPhilip7@users.noreply.github.com>
2021-06-18 21:48:13 +0200

    poprawki w kodzie

--- BubbleGame/Assets/Scripts/boundaries.cs
+++ BubbleGame/Assets/Scripts/boundaries.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Boundaries : MonoBehaviour {
-    public Camera MainCamera;
+    public Camera mainCamera;
     private Vector2 screenBounds;
     private float objectWidth;
     private float objectHeight;
 
     // Use this for initialization
     private void Start() {
-        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
         objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
         objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
     }
